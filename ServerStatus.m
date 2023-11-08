30874c935a1e2c9dca57dbcfcbb2dc8a832d9863
Siddy5303 <115711861+Siddy0060@users.noreply.github.com>
2023-11-07 23:20:51 -0800

    Update ServerStatus.m

--- ServerStatus.m
+++ ServerStatus.m
@@ -1,4 +1,9 @@
 #^server_id = 896-482-90 {
 status_holder.testweb("true.found")
-R&{testweb.deployment()}
+R&{testweb.deployment(pages-build-deployment{dynamic.workflow})}
+}
+#^server_id = 476-987-81 {
+status_holder.testweb("false.Mfound")
+R&{testweb.changes.m(pages-build-deployment{dynamic.workflow})}
+B&{testweb.workflow.run(m.code{.workflow})}
 }
